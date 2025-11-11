7ddd81b9ad23a06b1c0a02ffa1880bd9a19d948e
luckmer <woszczo04@gmail.com>
2025-11-11 17:21:27 +0100

    ant theme

---
+++ packages/ui/common/antTheme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  token: {
+    colorPrimary: '#52c41a',
+    borderRadius: 8,
+    fontSize: 16,
+  },
+  components: {
+    Drawer: {
+      colorBgElevated: '#111A1F',
+      colorBgMask: 'rgba(0, 0, 0, 0.65)',
+      colorText: '#fff',
+      colorIcon: '#fff',
+      colorIconHover: '#1B2830',
+    },
+  },
+}
