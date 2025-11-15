0d509464df26f0edc513b06311cebca0c23c23dd
luckmer <woszczo04@gmail.com>
2025-11-15 12:17:35 +0100

    connect book metadata

--- web/src/containers/Drawers/chaptersDrawer.tsx
+++ web/src/containers/Drawers/chaptersDrawer.tsx
@@ -1,12 +1,13 @@
 import ChaptersDrawer from '@pages/chaptersDrawer'
 import { actions as uiActions } from '@store/reducers/ui'
-import { bookSelector } from '@store/selectors/books'
+import { bookSelector, booksMapSelector } from '@store/selectors/books'
 import { uiSelector } from '@store/selectors/ui'
 import { useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 
 const ChaptersDrawerRoot = () => {
+  const booksMap = useSelector(booksMapSelector)
   const isOpen = useSelector(uiSelector.openChaptersDrawer)
   const tocMap = useSelector(bookSelector.toc)
 
@@ -16,17 +17,19 @@ const ChaptersDrawerRoot = () => {
   const hash = useMemo(() => location?.state?.id, [location])
 
   const toc = useMemo(() => tocMap[hash] ?? [], [hash, tocMap])
+  const book = useMemo(() => booksMap[hash], [hash, booksMap])
 
   return (
     <ChaptersDrawer
-      author=""
-      icon=""
-      title=""
+      author={book?.author ?? '--'}
+      icon={book?.metadata?.cover}
+      title={book?.title ?? '--'}
       toc={toc}
       isOpen={isOpen}
       onClickClose={() => {
         dispatch(uiActions.setOpenChaptersDrawer(false))
       }}
+      onClick={() => {}}
     />
   )
 }
