4991f5fa2682471408243fa746d89f1c74230d29
luckmer <woszczo04@gmail.com>
2026-01-12 20:25:44 +0100

    connect commands

--- web/src-tauri/src/main.rs
+++ web/src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use books::books_commands;
+use api::commands;
 use database::{service::init_database, structs::AppState};
 
 fn main() {
@@ -10,11 +10,7 @@ fn main() {
     let state = AppState { db: db_manager };
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            // books_commands::get_books,
-            // books_commands::add_book,
-            // books_commands::remove_book
-        ])
+        .invoke_handler(tauri::generate_handler![commands::get_books])
         .manage(state)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
