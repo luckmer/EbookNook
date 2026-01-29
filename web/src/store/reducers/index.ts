2770decf13a1a17481c00723c2626976b3ac8340
luckmer <woszczo04@gmail.com>
2026-01-29 20:17:51 +0100

    persist settings

--- web/src/store/reducers/index.ts
+++ web/src/store/reducers/index.ts
@@ -3,9 +3,12 @@ import { reducers as bookReducers, booksStore } from './books'
 import { reducers as searchReducers, searchStore } from './search'
 import { reducers as settingsReducers, settingsStore } from './settings'
 import { reducers as uiReducers, uiStore } from './ui'
+import { persistReducer } from 'redux-persist'
+import { persistSettings } from './settings/config'
+import { ISettingsState } from '@interfaces/settings/interfaces'
 
 const Index = combineReducers({
-  [settingsStore]: settingsReducers,
+  [settingsStore]: persistReducer<ISettingsState>(persistSettings, settingsReducers),
   [searchStore]: searchReducers,
   [booksStore]: bookReducers,
   [uiStore]: uiReducers,
