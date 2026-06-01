5b464d08dd96a921ee0cde865742520c764634fa
luckmer <woszczo04@gmail.com>
2026-06-01 19:08:17 +0200

    tests structure

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom', 
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+  },
+})
---
+++ vitest.workspace.ts
@@ -0,0 +1,1 @@
+export default ['packages/ui/vitest.config.ts', 'web/vitest.config.ts']
